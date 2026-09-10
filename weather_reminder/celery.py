0f46c1f9074ab979f6da6fdb900e51d94a84a82f
Yurii <yurijlevchuk@gmail.com>
2026-09-10 21:20:15 +0200

    Add production settings, static files and CSRF config for Cloud Run

--- weather_reminder/celery.py
+++ weather_reminder/celery.py
@@ -6,4 +6,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weather_reminder.settings")
 
 app = Celery("weather_reminder")
 app.config_from_object("django.conf:settings", namespace="CELERY")
+app.conf.broker_url = os.environ.get("BROKER_URL") or app.conf.broker_url
 app.autodiscover_tasks()
